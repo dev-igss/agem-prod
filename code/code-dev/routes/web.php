9120f55e1654971c5e13aef79fb56d0f64b3dfee
dev-igss <devigsshgq@gmail.com>
2026-03-26 15:28:54 -0600

    pruebas de tiempo session

--- code/code-dev/routes/web.php
+++ code/code-dev/routes/web.php
@@ -19,17 +19,6 @@ use App\Http\Controllers\PatientDayController;
 
 Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('login');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');   
-   
-});
-
 require __DIR__.'/auth.php';
 
 Route::prefix('admin')->group(function () {
