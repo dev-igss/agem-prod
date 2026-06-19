458c73eedd32c69320d7b4340b56108932d15153
dev-igss <devigsshgq@gmail.com>
2026-06-18 18:18:21 -0600

    Update AuthenticatedSessionController.php

--- code/code-dev/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ code/code-dev/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -24,7 +24,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        return $request;
+        return "prueba";
         /*$request->authenticate();
 
         $request->session()->regenerate();
